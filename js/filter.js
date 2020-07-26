fe2dea07db37fcfef6b5646be58c4150493b609f
claudiabdm <claudiabdm>
2020-07-26 20:54:26 +0200

    deploy

---
+++ js/filter.js
@@ -0,0 +1,71 @@
+// SORT CONTACT LIST A-Z
+// -----------------------------------------------------
+
+function sortAZ(a, b) {
+  if (a.name.toLowerCase() < b.name.toLowerCase()) {
+    return -1;
+  } else if (a.name > b.name) {
+    return 1;
+  } else {
+    if (a.surname < b.surname) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+}
+
+// ADD CLASS TO SELECTED FILTER
+// -----------------------------------------------------
+
+function selectedFilter(filter) {
+  const prevFilter = document.querySelector('.sidebar__elem--selected');
+  const currFilter = filter ? document.getElementById(filter) : document.getElementById('allContacts');
+  if (!prevFilter) {
+    currFilter.classList.add('sidebar__elem--selected');
+  } else {
+    prevFilter.classList.remove('sidebar__elem--selected');
+    currFilter.classList.add('sidebar__elem--selected');
+  }
+}
+
+
+// FILTER  CONTACTS BY GROUP
+// -----------------------------------------------------
+function filterContacts(contactListElem, contactInfoElem, contacts, filter) {
+  selectedFilter(filter);
+  if (filter === 'allContacts' || !filter) {
+    contacts.forEach((elem) => elem.visible = true);
+    paintContactList(contactListElem, contactInfoElem, contacts);
+  } else {
+    contacts.forEach((elem) => {
+      if (elem.group === filter) {
+        elem.visible = true;
+      } else {
+        elem.visible = false;
+      }
+    });
+  }
+}
+
+// FILTER CONTACTS BY TEXT INPUT
+// -----------------------------------------------------
+function searchContact(contactListElem, contactInfoElem, contacts, filter, e) {
+
+  const inputText = e.currentTarget.value;
+  contacts.forEach((elem) => {
+    const fullName = `${elem.name} ${elem.surname}`;
+    const regex = new RegExp(`${inputText}`, 'gi');
+    if (regex.test(fullName)) {
+      elem.visible = true;
+      if (elem.group === filter || filter === 'allContacts') {
+        elem.visible = true;
+      } else {
+        elem.visible = false;
+      }
+    } else {
+      elem.visible = false;
+    }
+  });
+  paintContactList(contactListElem, contactInfoElem, contacts);
+}
---
+++ js/modifyContactList.js
@@ -0,0 +1,65 @@
+// ADD CONTACT
+// -----------------------------------------------------
+function addContactToList(contactListElem, contactInfoElem, contacts) {
+  const name = document.getElementById('name');
+  const surname = document.getElementById('surname');
+  const email = document.getElementById('email');
+  const prefix = document.getElementById('prefix');
+  const phone = document.getElementById('phone');
+  const group = document.getElementById('group');
+  const randomColor = randomColors[Math.floor(Math.random() * randomColors.length)];
+
+  const contact = {
+    name: name.value,
+    surname: surname.value,
+    email: email.value,
+    country: prefix.value,
+    phone: phone.value,
+    group: group.value,
+    visible: true,
+    color: randomColor,
+    id: contacts.length,
+  };
+
+  contacts.push(contact);
+
+  filterContacts(contactListElem, contactInfoElem, contacts, contact.group);
+  paintContactList(contactListElem, contactInfoElem, contacts);
+  paintContactInfo(contactInfoElem, contacts, document.querySelector(`[data-id="${contact.id}"]`));
+}
+
+// MODIFY CONTACT
+// -----------------------------------------------------
+
+function updateContactInList(contactListElem, contactInfoElem, contacts) {
+  const contactId = parseInt(contactInfoElem.getAttribute('data-id'));
+  const contact = contacts.find((elem) => elem.id === contactId);
+  const name = document.getElementById('name');
+  const surname = document.getElementById('surname');
+  const email = document.getElementById('email');
+  const prefix = document.getElementById('prefix');
+  const phone = document.getElementById('phone');
+  const group = document.getElementById('group');
+
+  contact.name = name.value;
+  contact.surname = surname.value;
+  contact.email = email.value;
+  contact.country = prefix.value;
+  contact.phone = phone.value;
+  contact.group = group.value;
+
+  filterContacts(contactListElem, contactInfoElem, contacts, contact.group);
+  paintContactList(contactListElem, contactInfoElem, contacts);
+  paintContactInfo(contactInfoElem, contacts, document.querySelector(`[data-id="${contact.id}"]`));
+}
+
+
+// DELETE CONTACT
+// -----------------------------------------------------
+
+function deleteContact(contactListElem, contactInfoElem, contacts, e) {
+  const contactId = parseInt(e.currentTarget.parentElement.getAttribute('data-id'));
+  const contactIdx = contacts.findIndex((elem) => elem.id === contactId);
+  contacts.splice(contactIdx, 1);
+  paintContactList(contactListElem, contactInfoElem, contacts);
+}
